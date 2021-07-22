cc385bcc63e0edfd321b8c68ea732472cef7c88d
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-22 17:56:17 +0800

    feat: remove async factions

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,36 +1,26 @@
-const db = require('../models')
-const { User, Tweet, Like } = db
 const adminService = require('../services/adminService')
 
 const adminController = {
-  getUsers: async (req, res, next) => {
+  getUsers: (req, res, next) => {
     // #swagger.tags = ['Admin']
     // #swagger.description = 'Get users data.'
-    try {
-      adminService.getUsers(req, res, data => {
-        if (data.status === 'error') {
-          return res.status(404).json(data)
-        }
-        return res.status(200).json(data)
-      })
-    } catch (err) {
-      next(err)
-    }
+    adminService.getUsers(req, res, data => {
+      if (data.status === 'error') {
+        return res.status(404).json(data)
+      }
+      return res.status(200).json(data)
+    }).catch((err) => { next(err) })
   },
-  deleteTweet: async (req, res, next) => {
+  deleteTweet: (req, res, next) => {
     // #swagger.tags = ['Admin']
     // #swagger.description = 'Delete a tweet.'
-    try {
-      adminService.deleteTweet
-      (req, res, data => {
-        if (data.status === 'error') {
-          return res.status(401).json(data)
-        }
-        return res.status(200).json(data)
-      })
-    } catch (err) {
-      next(err)
-    }
+    adminService.deleteTweet
+    (req, res, data => {
+      if (data.status === 'error') {
+        return res.status(401).json(data)
+      }
+      return res.status(200).json(data)
+    }).catch((err) => { next(err) })
   }
 }
 
