5a2e81fadbc4c07f95cbb1b488974e8b32999c2e
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-22 17:37:23 +0800

    feat: adminService.getUsers feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,39 +1,18 @@
 const db = require('../models')
 const { User, Tweet, Like } = db
+const adminService = require('../services/adminService')
 
 const adminController = {
   getUsers: async (req, res, next) => {
     // #swagger.tags = ['Admin']
     // #swagger.description = 'Get users data.'
     try {
-      let users = await User.findAll({
-        include: [
-          { model: Tweet, include: [Like] },
-          { model: User, as: 'Followings' },
-          { model: User, as: 'Followers' }]
-      })
-      if (!users) {
-        return res.status(404).json({ status: 'error', message: 'Cannot find any users in db.' })
-      }
-      users = users.map(user => {
-        let likedCount = 0
-        user.Tweets.forEach(tweet => {
-          likedCount += tweet.Likes.length
-        })
-        return {
-          id: user.id,
-          account: user.account,
-          name: user.name,
-          avatar: user.avatar,
-          cover: user.cover,
-          tweetCount: user.Tweets.length,
-          likedCount,
-          followingCount: user.Followings.length,
-          followerCount: user.Followers.length
+      adminService.getUsers(req, res, data => {
+        if (data.status && data.status === 'error') {
+          return res.status(404).json(data.message)
         }
+        return res.status(200).json(data)
       })
-      users.sort((a, b) => b.tweetCount - a.tweetCount)
-      return res.status(200).json(users)
     } catch (err) {
       next(err)
     }
