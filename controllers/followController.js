dcd3c6a3b52dec05547a95ee85c40161ef6899c2
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-22 21:13:10 +0800

    feat: add followService.removeFollowing

--- controllers/followController.js
+++ controllers/followController.js
@@ -8,7 +8,7 @@ const followController = {
       if (data.status === 'error') {
         return res.status(404).json(data)
       }
-      if (data.status === 'forbidden') {
+      if (data.status === 'conflict') {
         return res.status(409).json(data)
       }
       if (data.status === 'forbidden') {
@@ -17,52 +17,21 @@ const followController = {
       return res.status(200).json(data)
     }).catch((err) => { next(err) })
   },
-  removeFollowing: async (req, res, next) => {
+  removeFollowing: (req, res, next) => {
     // #swagger.tags = ['Followships']
     // #swagger.description = 'Unfollow a user.'
-    try {
-      const followerId = req.user.id
-      const followingId = req.params.followingId
-
-      const unfollowingUser = await User.findOne({
-        where: {
-          id: followingId,
-          role: 'user'
-        }
-      })
-      // check this followerId's role should be user.
-      const unfollowerUser = await User.findOne({
-        where: {
-          id: followerId,
-          role: 'user'
-        }
-      })
-
-      if (!unfollowingUser || !unfollowerUser) {
-        return res.status(404).json({ status: 'error', message: 'Cannot find this followingId or followerId.' })
+    followService.removeFollowing(req, res, data => {
+      if (data.status === 'error') {
+        return res.status(404).json(data)
       }
-
-      if (followerId === Number(followingId)) {
-        return res.status(403).json({ status: 'error', message: 'You cannot unfollow yourself.' })
+      if (data.status === 'conflict') {
+        return res.status(409).json(data)
       }
-
-      const followship = await Followship.findOne({
-        where: {
-          followerId,
-          followingId
-        }
-      })
-
-      if (!followship) {
-        return res.status(409).json({ status: 'error', message: `You didn't followed @${unfollowingUser.account} before.` })
+      if (data.status === 'forbidden') {
+        return res.status(403).json(data)
       }
-
-      await followship.destroy()
-
-      return res.status(200).json({ status: 'success', message: `Unfollowed @${unfollowingUser.account} successfully.` })
-    } catch (err) {
-      next(err)
-    }
+      return res.status(200).json(data)
+    }).catch((err) => { next(err) })
   }
 }
 
--- services/followService.js
+++ services/followService.js
@@ -46,6 +46,45 @@ const followService = {
     } catch (err) {
       console.log(err)
     }
+  },
+  removeFollowing: async (req, res, callback) => {
+    try {
+      const followerId = req.user.id
+      const followingId = req.params.followingId
+
+      const unfollowingUser = await User.findOne({
+        where: {
+          id: followingId,
+          role: 'user'
+        }
+      })
+      // check this followerId's role should be user.
+      const unfollowerUser = await User.findOne({
+        where: {
+          id: followerId,
+          role: 'user'
+        }
+      })
+      if (!unfollowingUser || !unfollowerUser) {
+        return callback({ status: 'error', message: 'Cannot find this followingId or followerId.' })
+      }
+      if (followerId === Number(followingId)) {
+        return callback({ status: 'forbidden', message: 'You cannot unfollow yourself.' })
+      }
+      const followship = await Followship.findOne({
+        where: {
+          followerId,
+          followingId
+        }
+      })
+      if (!followship) {
+        return callback({ status: 'conflict', message: `You didn't followed @${unfollowingUser.account} before.` })
+      }
+      await followship.destroy()
+      return callback({ status: 'success', message: `Unfollowed @${unfollowingUser.account} successfully.` })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
 }
