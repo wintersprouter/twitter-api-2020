501b13c965234436ef196afa9eccb3aca38e8e43
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-23 00:04:47 +0800

    feat: add adminService.getUsers

--- services/adminService.js
+++ services/adminService.js
@@ -1,10 +1,12 @@
 const db = require('../models')
 const { User, Tweet, Like } = db
+const organizeData = require('../utils/organizeData')
+const { getUsersData } = organizeData
 
 const adminService = {
   getUsers: async (req, res, callback) => {
     try {
-      let users = await User.findAll({
+      const users = await User.findAll({
         include: [
           { model: Tweet, include: [Like] },
           { model: User, as: 'Followings' },
@@ -13,29 +15,9 @@ const adminService = {
       if (!users) {
         return callback({ status: 'error', message: 'Cannot find any users in db.' })
       }
-
-      users = users.map(user => {
-        let likedCount = 0
-        user.Tweets.forEach(tweet => {
-          likedCount += tweet.Likes.length
-        })
-        return {
-          id: user.id,
-          account: user.account,
-          name: user.name,
-          avatar: user.avatar,
-          cover: user.cover,
-          tweetCount: user.Tweets.length,
-          likedCount,
-          followingCount: user.Followings.length,
-          followerCount: user.Followers.length
-        }
-      })
-      users.sort((a, b) => b.tweetCount - a.tweetCount)
-      let data
-      data = users
-      data.status = 'success'
-      return callback(data)
+      const usersData = await getUsersData(req, users)
+      // usersData.status = 'success'
+      return callback(usersData)
     } catch (err) {
       console.log(err)
     }
