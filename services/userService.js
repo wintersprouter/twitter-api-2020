392aaa6046841df172adac0de30fce6cfcec6e97
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-23 16:25:59 +0800

    feat: add fromValidation.js

--- services/userService.js
+++ services/userService.js
@@ -7,6 +7,8 @@ const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const organizeData = require('../utils/organizeData')
 const { getSignInData } = organizeData
+const fromValidation = require('../utils/fromValidation')
+const { checkUserInfo } = fromValidation
 
 const uploadImg = path => {
   return new Promise((resolve, reject) => {
@@ -47,42 +49,13 @@ const userService = {
   },
   signUp: async (req, res, callback) => {
     try {
-      const { account, name, email, password, checkPassword } = req.body
       const message = []
-      // check all inputs are required
-      if (!account || !name || !email || !password || !checkPassword) {
-        message.push('All fields are required！')
-      }
-      // check name length and type
-      if (name && !validator.isByteLength(name, { min: 0, max: 50 })) {
-        message.push('Name can not be longer than 50 characters.')
-      }
-      // check account length and type
-      if (account && !validator.isByteLength(account, { min: 0, max: 20 })) {
-        message.push('Account can not be longer than 20 characters.')
-      }
-      // check email validation
-      if (email && !validator.isEmail(email)) {
-        message.push(`${email} is not a valid email address.`)
-      }
-      // check password length and type
-      if (password && !validator.isByteLength(password, { min: 5, max: 15 })) {
-        message.push('Password does not meet the required length.')
-      }
-      // check password and checkPassword
-      if (password && (password !== checkPassword)) {
-        message.push('The password and confirmation do not match.Please retype them.')
-      }
-      const [inputEmail, inputAccount] = await Promise.all([User.findOne({ where: { email } }), User.findOne({ where: { account } })])
-      if (inputEmail) {
-        message.push('This email address is already being used.')
-      }
-      if (inputAccount) {
-        message.push('This account is already being used.')
-      }
+      await checkUserInfo(req, message)
+
       if (message.length) {
         return callback({ status: 'error', message })
       }
+      const { account, name, email, password } = req.body
       await User.create({
         account,
         name,
---
+++ utils/fromValidation.js
@@ -0,0 +1,48 @@
+const db = require('../models')
+const { User } = db
+const validator = require('validator')
+
+module.exports = {
+  checkUserInfo: async (req, message) => {
+    try {
+      const { account, name, email, password, checkPassword } = req.body
+
+      // check all inputs are required
+      if (!account || !name || !email || !password || !checkPassword) {
+        message.push('All fields are required！')
+      }
+      // check name length and type
+      if (name && !validator.isByteLength(name, { min: 0, max: 50 })) {
+        message.push('Name can not be longer than 50 characters.')
+      }
+      // check account length and type
+      if (account && !validator.isByteLength(account, { min: 0, max: 20 })) {
+        message.push('Account can not be longer than 20 characters.')
+      }
+      // check email validation
+      if (email && !validator.isEmail(email)) {
+        message.push(`${email} is not a valid email address.`)
+      }
+      // check password length and type
+      if (password && !validator.isByteLength(password, { min: 5, max: 15 })) {
+        message.push('Password does not meet the required length.')
+      }
+      // check password and checkPassword
+      if (password && (password !== checkPassword)) {
+        message.push('The password and confirmation do not match.Please retype them.')
+      }
+      const [inputEmail, inputAccount] = await Promise.all([User.findOne({ where: { email } }), User.findOne({ where: { account } })])
+      if (inputEmail) {
+        message.push('This email address is already being used.')
+      }
+      if (inputAccount) {
+        message.push('This account is already being used.')
+      }
+      if (message.length) {
+        return message
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
