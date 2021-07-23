c752e4dececdb172e85e3ad85019b9440a2fde41
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-23 16:10:58 +0800

    feat: add userService.signUp

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,56 +14,15 @@ const userController = {
       return res.status(200).json(data)
     }).catch((err) => { next(err) })
   },
-  signUp: async (req, res, next) => {
+  signUp: (req, res, next) => {
     // #swagger.tags = ['SignUp/Signin']
     // #swagger.description = 'User sign up.'
-    try {
-      const { account, name, email, password, checkPassword } = req.body
-      const message = []
-      // check all inputs are required
-      if (!account || !name || !email || !password || !checkPassword) {
-        message.push('All fields are required！')
+    userService.signUp(req, res, data => {
+      if (data.status === 'error: bad request') {
+        return res.status(400).json(data)
       }
-      // check name length and type
-      if (name && !validator.isByteLength(name, { min: 0, max: 50 })) {
-        message.push('Name can not be longer than 50 characters.')
-      }
-      // check account length and type
-      if (account && !validator.isByteLength(account, { min: 0, max: 20 })) {
-        message.push('Account can not be longer than 20 characters.')
-      }
-      // check email validation
-      if (email && !validator.isEmail(email)) {
-        message.push(`${email} is not a valid email address.`)
-      }
-      // check password length and type
-      if (password && !validator.isByteLength(password, { min: 5, max: 15 })) {
-        message.push('Password does not meet the required length.')
-      }
-      // check password and checkPassword
-      if (password && (password !== checkPassword)) {
-        message.push('The password and confirmation do not match.Please retype them.')
-      }
-      const [inputEmail, inputAccount] = await Promise.all([User.findOne({ where: { email } }), User.findOne({ where: { account } })])
-      if (inputEmail) {
-        message.push('This email address is already being used.')
-      }
-      if (inputAccount) {
-        message.push('This account is already being used.')
-      }
-      if (message.length) {
-        return res.status(400).json({ status: 'error', message })
-      }
-      await User.create({
-        account,
-        name,
-        email,
-        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-      })
-      return res.status(200).json({ status: 'success', message: `@${account} sign up successfully.Please sign in.` })
-    } catch (err) {
-      next(err)
-    }
+      return res.status(200).json(data)
+    }).catch((err) => { next(err) })
   },
 
   getCurrentUser: (req, res, next) => {
--- services/userService.js
+++ services/userService.js
@@ -44,6 +44,55 @@ const userService = {
     } catch (err) {
       console.log(err)
     }
+  },
+  signUp: async (req, res, callback) => {
+    try {
+      const { account, name, email, password, checkPassword } = req.body
+      const message = []
+      // check all inputs are required
+      if (!account || !name || !email || !password || !checkPassword) {
+        message.push('All fields are required！')
+      }
+      // check name length and type
+      if (name && !validator.isByteLength(name, { min: 0, max: 50 })) {
+        message.push('Name can not be longer than 50 characters.')
+      }
+      // check account length and type
+      if (account && !validator.isByteLength(account, { min: 0, max: 20 })) {
+        message.push('Account can not be longer than 20 characters.')
+      }
+      // check email validation
+      if (email && !validator.isEmail(email)) {
+        message.push(`${email} is not a valid email address.`)
+      }
+      // check password length and type
+      if (password && !validator.isByteLength(password, { min: 5, max: 15 })) {
+        message.push('Password does not meet the required length.')
+      }
+      // check password and checkPassword
+      if (password && (password !== checkPassword)) {
+        message.push('The password and confirmation do not match.Please retype them.')
+      }
+      const [inputEmail, inputAccount] = await Promise.all([User.findOne({ where: { email } }), User.findOne({ where: { account } })])
+      if (inputEmail) {
+        message.push('This email address is already being used.')
+      }
+      if (inputAccount) {
+        message.push('This account is already being used.')
+      }
+      if (message.length) {
+        return callback({ status: 'error', message })
+      }
+      await User.create({
+        account,
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+      })
+      return callback({ status: 'success', message: `@${account} sign up successfully.Please sign in.` })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
 }
